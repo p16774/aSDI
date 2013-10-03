061c0b4231c78bb0a1cfee8569676a2d08887612
Nathan Wharry <nobrakes@fullsail.edu>
2013-10-03 18:48:12 -0500

    Initial Master setup
    Setup initial files for the rest of the week for aSDI class.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+/*
+
+*********************
+Author: Nathan Wharry
+Title: Project 1 - Blood Glucose Reporting Tool - Javascript
+Term: aSDI 1310
+*********************
+
+*/
+
+// Run content on page load only
+$(#mainForm input).on("click", function() {
+	
+	//page content for the index form will go here
+	
+});
