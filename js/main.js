183c9f370c77b188680112eec834994a9d9faf7a
Nathan Wharry <nobrakes@fullsail.edu>
2013-10-31 21:18:32 -0500

    Revision after delete
    Deleted repo and rebuilt

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+/*
+
+*********************
+Author: Nathan Wharry
+Title: Project 1 - Video Games Wishlist - Javascript
+Term: aSDI 1311
+*********************
+
+*/
+
+// content for the add games page
+$('#games').on('pageshow', function() {
+	
+	// insert form building or form editing elements here	
+});
+
+// content for the wishlist page
+$('#games').on('pageshow', function() {
+	
+	// insert display elements here	
+});
